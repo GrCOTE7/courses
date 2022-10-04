3d8bbf0617ec0cf59b6867f5a86445ddfd585086
Lionel COTE <grcote7@gmail.com>
2022-10-04 09:24:46 +0200

    Feat: SQL requests for Migrations

---
+++ Courses_mysql_create.sql
@@ -0,0 +1,47 @@
+CREATE TABLE `users` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`is_admin` BOOLEAN NOT NULL,
+	`name` varchar(255) NOT NULL UNIQUE,
+	`password` varchar(255) NOT NULL,
+	`organization_id` INT(11) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `organization` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `courses` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	`category` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `course_user` (
+	`course_id` INT(11) NOT NULL,
+	`user_id` INT(11) NOT NULL
+);
+
+CREATE TABLE `course_organization` (
+	`course_id` INT(11) NOT NULL,
+	`organisation_id` INT(11) NOT NULL
+);
+
+ALTER TABLE `users` ADD CONSTRAINT `users_fk0` FOREIGN KEY (`organization_id`) REFERENCES `organization`(`id`);
+
+ALTER TABLE `course_user` ADD CONSTRAINT `course_user_fk0` FOREIGN KEY (`course_id`) REFERENCES `courses`(`id`);
+
+ALTER TABLE `course_user` ADD CONSTRAINT `course_user_fk1` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+
+ALTER TABLE `course_organization` ADD CONSTRAINT `course_organization_fk0` FOREIGN KEY (`course_id`) REFERENCES `courses`(`id`);
+
+ALTER TABLE `course_organization` ADD CONSTRAINT `course_organization_fk1` FOREIGN KEY (`organisation_id`) REFERENCES `organization`(`id`);
+
+
+
+
+
+
